e50bf269e8b57582323510d9fb21eee2d66d35c0
Oskar-Ciebien <oskarciebien@gmail.com>
2022-02-27 12:42:54 +0000

    Completed Obstacle Spawner script and set new Spawner GameObjects in GameScene

--- Dropper Speech Recognition Game/Assets/__Scripts/ObstacleSpawner.cs
+++ Dropper Speech Recognition Game/Assets/__Scripts/ObstacleSpawner.cs
@@ -5,16 +5,42 @@ using UnityEngine;
 public class ObstacleSpawner : MonoBehaviour
 {
     // == Public Fields ==
-    public Transform[] spawners;
-    public GameObject obstaclePrefab;
+    public GameObject[] spawnerPositions;
+    public GameObject spawnerPrefab;
+
+    // == Private Fields ==
+    public float startingSpawnTime = 3f;
+    private float spawnTime;
+
+    void Start()
+    {
+        // Set spawn time
+        spawnTime = startingSpawnTime;
+    }
 
     void Update()
     {
-
+        SpawnObstacle();
     }
 
     void SpawnObstacle()
     {
+        // If spawn time has reached 0 or less
+        if (spawnTime <= 0)
+        {
+            // Set a random spawn point
+            int randomSpawnPoint = Random.Range(0, spawnerPositions.Length);
 
+            // Spawn an obstacle at that random position
+            Instantiate(spawnerPrefab, spawnerPositions[randomSpawnPoint].transform.position, transform.rotation);
+
+            // Set the spawn time back to the starting time
+            spawnTime = startingSpawnTime;
+        }
+        else // Otherwise
+        {
+            // Decrease the spawn time
+            spawnTime -= Time.deltaTime;
+        }
     }
 }
