986f20def233dcfc32ec844e1320769c7283f415
Oskar-Ciebien <oskarciebien@gmail.com>
2022-03-04 13:05:31 +0000

    Added GoMainMenu() to Death script

--- Dropper Speech Recognition Game/Assets/__Scripts/DeathMenu/DeathMenuController.cs
+++ Dropper Speech Recognition Game/Assets/__Scripts/DeathMenu/DeathMenuController.cs
@@ -1,18 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DeathMenuController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void GoMainMenu()
     {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Loads the Main Menu
+        SceneManager.LoadScene("MainMenu");
     }
 }
