f6f8d538a303a5550ca7aad4565dfb01f7b51598
Oskar-Ciebien <oskarciebien@gmail.com>
2022-03-16 13:26:29 +0000

    Moved Downloaded Assets to a new folder and Added a new obstacle

---
+++ Dropper Speech Recognition Game/Assets/__Scripts/ObstacleS/ObstacleRotatation.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleRotatation : MonoBehaviour
+{
+    // == Private Fields ==
+    [SerializeField] private float rotation = 0.2f;
+
+    void FixedUpdate()
+    {
+        // Rotate Obstacle
+        gameObject.transform.Rotate(0, 0, rotation);
+    }
+}
